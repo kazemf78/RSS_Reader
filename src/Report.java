8a70267106f30d013e01871148733c623884f130
kazemf78 <kazemf78@yahoo.com>
2019-06-30 12:45:14 +0430

    add basic classes to fetch RSS

---
+++ src/Report.java
@@ -0,0 +1,37 @@
+import java.net.URL;
+
+public class Report {
+    String title;
+    URL link;
+    String description;
+
+    public Report(String title, URL link, String description) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public URL getLink() {
+        return link;
+    }
+
+    public void setLink(URL link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
